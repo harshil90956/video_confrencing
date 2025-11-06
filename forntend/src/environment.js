1b4788d63878672a2a3d75697bb171b0066677c8
Harshil Dobariya <143210326+harshil90956@users.noreply.github.com>
2025-11-06 10:43:09 +0530

    Toggle server URL for production and local use

--- forntend/src/environment.js
+++ forntend/src/environment.js
@@ -1,4 +1,4 @@
-// const server = "https://video-confrencing-b9py.onrender.com";
+const server = "https://video-confrencing-b9py.onrender.com";
 
-const server = "http://localhost:8000";
+// const server = "http://localhost:8000";
 export default server;
